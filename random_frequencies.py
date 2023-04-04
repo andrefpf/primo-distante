a9b429b2199390aa5998743daf08508fb730a032
André Fernandes <fpf.andre@gmail.com>
2023-04-04 13:39:23 -0300

    plot random frequencies

---
+++ random_frequencies.py
@@ -0,0 +1,24 @@
+from random_generator import xorshift_32, lcg_32
+import matplotlib.pyplot as plt
+
+def create_histogram(data, num_classes):
+    histogram = [0 for i in range(num_classes)]
+    for i in data:
+        bucket = i * num_classes // (1 << 32)
+        histogram[bucket] += 1
+    return histogram
+
+
+def evaluate_algorithm(algorithm, seed, repetitions):
+    generator = algorithm(seed)
+    random_values = [next(generator) for i in range(repetitions)]
+    return create_histogram(random_values, 50)
+
+
+xorshift_frequency = evaluate_algorithm(xorshift_32, 1234, 10_000)
+lcg_frequency = evaluate_algorithm(lcg_32, 1234, 10_000)
+
+x = range(len(xorshift_frequency))
+plt.bar(x, xorshift_frequency)
+plt.bar(x, lcg_frequency)
+plt.show()
